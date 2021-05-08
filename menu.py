cf07685823990f872f57d317d5721ee53fecf9c1
pmh-projects <77459300+pmh-projects@users.noreply.github.com>
2021-05-08 20:12:30 +0200

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,24 @@
+def menu():
+    print(
+        "Podstawowe funkcje:"
+        "\nwikipedia - uruchamia funkcję wyszukiwania haseł na wikipedii z możliwością zapisu do pliku"
+        "\nnotatka - funkcja zapisująca plik o podanym głosowo tytule i zawartością"
+        "\n'otwórz plik' lub 'otwórz notatkę' - otwieranie zapisanego pliku"
+        "\nklawiatura - funkcjaa umożliwiająca wywoływanie pojedyńczych klawiszy i skrótów klawiszowych"
+        "\ngoogle - wyszukuje 5 pierwszych wyników i uruchamia wyszukiwarkę z podanych hasłem"
+        "\nscreenshot - pobiera i zapisuje zawartość ekranu do pliku png z podanym przez użytkownika tytułem"
+        "\n"
+        "\nFunkcje NET:"
+        "\nStrona uniwersytetu - uruchamia stronę główną UG."
+        "\nstrona wydziału - uruchamia stronę wydziału WZR"
+        "\nportal edukacyjny - uruchamia stronę portalu edukacyjnego"
+        "\nportal studenta - uruchamia stronę portalu studenta"
+        "\nwyszukiwarka - otwiera wyszukiwarkę bez deklaracji hasła."
+        "\npomoc - masz problem z kodem? Otworzę stackoverflow."
+        "\notwórz youtube - chcesz posłuchać muzyki?"
+        "\n"
+        "\nFunkcje dodatkowe:"
+        '\npogoda "miasto"- zapytaj o aktualną pogodę wypowiadając funkcję z nazwą miejscowości, która chcesz sprawdzić'
+        "\nlotto - symulator lotto. Zastanawiałeś się jakie masz szczęście w grach losowych? Sprawdź się."
+        "\ngra - rozerwij się i ogadnij wylosowaną liczbę"
+        "\nzamknij - wyjście z programu.")
